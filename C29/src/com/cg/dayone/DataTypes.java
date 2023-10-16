0d2fa8b978550ae645162dc13bc6d873b4f9c506
TNSIF India <TNSIF India@REVEN-PC>
2023-10-16 15:27:30 +0530

    First commit

---
+++ C29/src/com/cg/dayone/DataTypes.java
@@ -0,0 +1,22 @@
+package com.cg.dayone;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		//datatypes and variables
+		int a = 102;
+		float b = 104.3f;
+		double c = 33.5;
+		boolean d = true;
+		char e = 'a';
+		
+		//print the variables 
+		System.out.println(a);
+		System.out.println("Integer is: "+a+" End of Statement");
+		System.out.println("Float is: "+b);
+		System.out.println("Double is: "+c);
+		System.out.println("Boolean is: "+d);
+		System.out.println("Character is: "+e);
+	}
+
+}
